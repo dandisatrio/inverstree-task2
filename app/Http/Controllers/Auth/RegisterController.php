22c2ca674e0ef10a36ec1196bb052c4ecdd5a030
dandisatrio <dandisatrio810@gmail.com>
2022-05-30 22:00:01 +0700

    page register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,8 +29,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = "/login";
-
+    protected $redirectTo = RouteServiceProvider::HOME;
+    
     /**
      * Create a new controller instance.
      *
