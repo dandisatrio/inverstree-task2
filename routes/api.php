193662f1fd987e7f407c01c921606a47f38287c3
dandisatrio <dandisatrio810@gmail.com>
2022-06-02 21:40:55 +0700

    create rest api category

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\api\v1\CategoryController;
 use App\Http\Controllers\api\v1\LoginController;
+use App\Http\Controllers\api\v1\PostController;
 use App\Http\Controllers\api\v1\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,10 +22,18 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/register', [RegisterController::class, 'register']);
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:api');
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('category' , [CategoryController::class, 'index']);
+    Route::post('category/store', [CategoryController::class, 'store']);
+    Route::get('category/{id}', [CategoryController::class, 'show']);
+    Route::post('category/update/{id}', [CategoryController::class, 'update']);
+    Route::delete('category/destroy/{id}', [CategoryController::class, 'destroy']);
+});
